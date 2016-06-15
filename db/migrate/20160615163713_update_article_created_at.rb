8e5b2f86947b71b6eba810742893ed73c769362d
techbender-leslie <techbenderleslie@gmail.com>
2016-06-15 10:12:06 -0700

    redid database

--- db/migrate/20160615163713_update_article_created_at.rb
+++
@@ -1,8 +0,0 @@
-class UpdateArticleCreatedAt < ActiveRecord::Migration
-  def change
-  	change_table :articles do |t|
-  		t.remove :created_at, :updated_at
-  		t.timestamps
-  	end
-  end
-end
---
+++ db/migrate/20160615170603_add_columns_to_articles.rb
@@ -0,0 +1,4 @@
+class AddColumnsToArticles < ActiveRecord::Migration
+  def change	
+  end
+end
