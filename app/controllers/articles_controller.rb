0c809450653d90eca2d3f8336cfbe1da21a67e84
techbender-leslie <techbenderleslie@gmail.com>
2016-06-20 00:07:10 -0700

    add authentication for articles and users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
-	before_action :set_article, only: [:edit, :update, :show, :destroy]
+	#these run in order
+	before_action :set_article, only: [:edit, :update, :show, :destroy]  #setting @article
+	before_action :require_user, except: [:index, :show]  #logged in
+	before_action :require_same_user, only: [:edit, :update, :destroy]  #owner
 
 	def index
 		@articles = Article.paginate(page: params[:page], per_page: 5)
@@ -48,12 +51,21 @@ class ArticlesController < ApplicationController
 
 
 	private
+		def set_article
+			@article = Article.find(params[:id])
+		end
+
 		def article_params
 			params.require(:article).permit(:title, :description)
 		end
 
-		def set_article
-			@article = Article.find(params[:id])
+		def require_same_user
+			if current_user != @article.user
+				flash[:danger] = "You can only edit or delete your own articles"
+				redirect_to root_path
+			end
 		end
+
+		
 
 end
